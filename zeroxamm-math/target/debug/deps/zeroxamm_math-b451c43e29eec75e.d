b5cd00fbb8e3f04be21aae7f2881e264f44066cf
srivtx <crypticcc101@gmail.com>
2026-05-26 04:39:34 +0530

    clean build artifacts from repo

--- zeroxamm-math/target/debug/deps/zeroxamm_math-b451c43e29eec75e.d
+++
@@ -1,5 +0,0 @@
-/Users/zen/Desktop/life-1/rsol/zeroxamm-math/target/debug/deps/zeroxamm_math-b451c43e29eec75e.d: src/lib.rs
-
-/Users/zen/Desktop/life-1/rsol/zeroxamm-math/target/debug/deps/libzeroxamm_math-b451c43e29eec75e.rmeta: src/lib.rs
-
-src/lib.rs:
--- zeroxamm-math/target/debug/deps/zeroxamm_math-d50a7c3688ffbaa5.d
+++
@@ -1,5 +0,0 @@
-/Users/zen/Desktop/life-1/rsol/zeroxamm-math/target/debug/deps/zeroxamm_math-d50a7c3688ffbaa5.d: src/lib.rs
-
-/Users/zen/Desktop/life-1/rsol/zeroxamm-math/target/debug/deps/libzeroxamm_math-d50a7c3688ffbaa5.rmeta: src/lib.rs
-
-src/lib.rs:
